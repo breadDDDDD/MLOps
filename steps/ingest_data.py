2504aa314337131b87e43b750e39f726c5739180
geervan thanra <geervanthanra1@gmail.com>
2025-01-12 19:52:16 +0700

    ingest data from csv into df with logs, adding csv, config for zenml

---
+++ steps/ingest_data.py
@@ -0,0 +1,21 @@
+import logging
+from zenml import step
+import numpy as np
+import pandas as pd
+
+class ingest_data:
+    def __init__(self, data_path : str):
+        self.data_path = data_path
+    
+    def get_all(self):
+        logging.info("ingesting data")
+        return pd.read_csv(self.data_path)
+@step
+def ingestData(data_path : str) -> pd.DataFrame:
+    try:
+        ingestData = ingest_data(data_path)
+        df = ingestData.get_all()
+        return df
+    except Exception as e:
+        logging.error("error :{e}")
+        raise e
